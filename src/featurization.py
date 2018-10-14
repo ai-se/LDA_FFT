e7d7ef69ade38ae010b22ccc13e0700444474e21
huytu7 <huyqtu7@gmail.com>
2018-10-14 16:32:36 -0400

    parallel processing for LDA

--- src/featurization.py
+++ src/featurization.py
@@ -8,21 +8,26 @@ import sys
 from sklearn.feature_extraction.text import HashingVectorizer,CountVectorizer,TfidfVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 
+
 def TF(doc):
-    vect= CountVectorizer(max_df=0.95, min_df=2,)
-    X= vect.fit_transform(doc)
-    return X.A,vect.get_feature_names()
+    vect = CountVectorizer(max_df=0.95, min_df=2,)
+    X = vect.fit_transform(doc)
+    return X.A, vect.get_feature_names()
+
 
 def TFIDF(doc):
     vect= TfidfVectorizer()
     X= vect.fit_transform(doc)
     return X.A, vect.get_feature_names()
 
+
 def HASHING(doc):
     vect= HashingVectorizer(n_features=1000)
     X= vect.fit_transform(doc)
     return X.A, ''
 
+
 def LDA_(doc,**k):
-    corpus, vocab=TF(doc)
-    return LatentDirichletAllocation(max_iter=1000,learning_method='online',**k).fit_transform(corpus), vocab
+    corpus, vocab = TF(doc)
+    lda_model = LatentDirichletAllocation(n_jobs=-1, max_iter=1000, learning_method='online', **k).fit_transform(corpus)
+    return lda_model, vocab
